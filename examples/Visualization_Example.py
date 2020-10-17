dc9e89adc5839ba115e0c9b0e3d14d5b3211a2e1
mmenzel <m.menzel@fz-juelich.de>
2020-10-17 11:43:53 +0200

    local changes

--- examples/Visualization_Example.py
+++ examples/Visualization_Example.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python
+#%%
 from SLIX import toolbox, visualization
 from matplotlib import pyplot as plt
 import numpy
 
 import urllib.request
-
+#%%
 # Download example image for visualization
 url = 'https://object.cscs.ch/v1/AUTH_227176556f3c4bb38df9feea4b91200c/hbp-d000048_ScatteredLightImaging_pub' \
       '/Human_Brain/optic_tracts_crossing_sections/SLI-human-Sub-01_2xOpticTracts_s0037_30um_SLI_105_Stack_3days_' \
       'registered.nii'
 urllib.request.urlretrieve(url, './SLI-human-Sub-01_2xOpticTracts_s0037_30um_SLI_105_Stack_3days_registered.nii')
 
+#%%
 # Read image in toolbox and create direction map
 image = toolbox.read_image('./SLI-human-Sub-01_2xOpticTracts_s0037_30um_SLI_105_Stack_3days_registered.nii')
+#%%
 roiset = toolbox.create_roiset(image, 1)
 direction = toolbox.crossing_direction_image(roiset)
+#%%
 # Reconstruct direction map to image for visualization
 direction_image = direction.reshape((image.shape[0], image.shape[1], 3))
 # Create unit vectors
 UnitX, UnitY = visualization.unit_vectors(direction_image)
+#%%
 # Show unit vectors with image in one python plot
 plt.imshow(numpy.mean(image, axis=-1), cmap='gray')
-visualization.visualize_unit_vectors(UnitX, UnitY, thinout=20, alpha=0.3)
+visualization.visualize_unit_vectors(UnitX, UnitY, thinout=10, alpha=1)
 plt.axis('off')
 plt.show()
 
 
 
 
+
+# %%
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=['SLIX'],
     url='www.github.com/3d-pli/slix',
     license='MIT',
-    author='Jan André Reuter, Miriam Menzel',
+    author='Jan Andre Reuter, Miriam Menzel',
     author_email='j.reuter@fz-juelich.de',
     description='SLIX allows an automated evaluation of SLI measurements and generates different parameter maps.',
     scripts=['bin/SLIXParameterGenerator', 'bin/SLIXLineplotParameterGenerator'],
