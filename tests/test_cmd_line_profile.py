24947141fa334164f9fd35039219cff2fd875dd3
Jan André Reuter <jan.andre.reuter@hotmail.de>
2020-11-02 15:28:15 +0100

    Added line plot command line test
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- tests/test_cmd_line_profile.py
+++ tests/test_cmd_line_profile.py
@@ -1,6 +1,9 @@
+import csv
+import os
 import pytest
-import argparse
 import shlex
+import shutil
+from unittest import mock
 from SLIX import cmd_line_profile
 
 
@@ -119,4 +122,68 @@ class TestCommandLineProfile:
         assert args['optional'] == True
 
     def test_main(self):
-        pass
+        with mock.patch('sys.argv', ['SLIXLineplotParameterGenerator',
+                                     '--input',
+                                     'examples/90-Stack-1647-1234.txt',
+                                     '--output',
+                                     'tests/files/output/',
+                                     '--optional',
+                                     '--with_plots']):
+            cmd_line_profile.main()
+        assert os.path.isdir('tests/files/output/')
+        assert os.path.isfile('tests/files/output/90-Stack-1647-1234.csv')
+        assert os.path.isfile('tests/files/output/90-Stack-1647-1234.png')
+
+        with open('tests/files/output/90-Stack-1647-1234.csv', newline='\n') as f:
+            reader = csv.reader(f, delimiter=',')
+            list_of_attrs = ['High Prominence Peaks',
+                             'Low Prominence Peaks',
+                             'Mean Prominence',
+                             'Mean peak width',
+                             'Mean peak distance',
+                             'Direction',
+                             'Min',
+                             'Max',
+                             'Avg']
+            attr = 0
+            for row in reader:
+                assert row[0] == list_of_attrs[attr]
+                attr += 1
+
+        with mock.patch('sys.argv', ['SLIXLineplotParameterGenerator',
+                                     '--input',
+                                     'examples/90-Stack-1647-1234.txt',
+                                     '--output',
+                                     'tests/files/output/second/']):
+            cmd_line_profile.main()
+        assert os.path.isdir('tests/files/output/second/')
+        assert os.path.isfile('tests/files/output/second/90-Stack-1647-1234.csv')
+        assert not os.path.isfile('tests/files/output/second/90-Stack-1647-1234.png')
+
+        with open('tests/files/output/second/90-Stack-1647-1234.csv', newline='\n') as f:
+            reader = csv.reader(f, delimiter=',')
+            list_of_attrs = ['High Prominence Peaks',
+                             'Low Prominence Peaks',
+                             'Mean Prominence',
+                             'Mean peak width',
+                             'Mean peak distance',
+                             'Direction']
+            attr = 0
+            for row in reader:
+                assert row[0] == list_of_attrs[attr]
+                attr += 1
+
+@pytest.fixture(scope="session", autouse=True)
+def run_around_tests(request):
+    # Code that will run before your test, for example:
+    assert os.path.isfile('examples/90-Stack-1647-1234.txt')
+    assert os.path.isfile('examples/90-Stack-2481-1524.txt')
+
+    # A test function will be run at this point
+    yield
+
+    # Code that will run after your test, for example:
+    def remove_test_dir():
+        if os.path.isdir('tests/files/output/'):
+            shutil.rmtree('tests/files/output/')
+    request.addfinalizer(remove_test_dir)
