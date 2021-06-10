c395f348805cd477a40c4999f01cfa93c6aa0862
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-06-10 14:46:21 +0200

    Improved visualization tests
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,3 +1,4 @@
+import numpy
 from matplotlib.testing.decorators import image_comparison
 from SLIX import toolbox, io, visualization
 import matplotlib
@@ -25,6 +26,40 @@ class TestVisualization:
         unit_x, unit_y = toolbox.unit_vectors(direction, use_gpu=False)
         visualization.visualize_unit_vectors(unit_x, unit_y, thinout=10)
 
+    def test_visualize_direction_one_dir(self):
+        image = numpy.arange(0, 180)
+        hsv_image = visualization.visualize_direction(image)
+        assert numpy.all(hsv_image[0, :] == [1, 0, 0])
+        assert numpy.all(hsv_image[30, :] == [1, 1, 0])
+        assert numpy.all(hsv_image[60, :] == [0, 1, 0])
+        assert numpy.all(hsv_image[90, :] == [0, 1, 1])
+        assert numpy.all(hsv_image[120, :] == [0, 0, 1])
+        assert numpy.all(hsv_image[150, :] == [1, 0, 1])
+
+    def test_visualize_direction_multiple_dir(self):
+        image = numpy.arange(0, 180)[..., numpy.newaxis, numpy.newaxis]
+        stack_direction = numpy.concatenate((image,
+                                             image + 90 % 180,
+                                             numpy.full(image.shape, -1)),
+                                            axis=-1)
+        hsv_image = visualization.visualize_direction(stack_direction)
+
+        assert numpy.all(hsv_image[0, 0, :] == [1, 0, 0])
+        assert numpy.all(hsv_image[1, 1, :] == [1, 0, 0])
+        # One check should be enough
+        assert numpy.all(hsv_image[0, 1, :] == [0, 1, 1])
+        assert numpy.all(hsv_image[1, 0, :] == [0, 1, 1])
+        assert numpy.all(hsv_image[60, 0, :] == [1, 1, 0])
+        assert numpy.all(hsv_image[61, 1, :] == [1, 1, 0])
+        assert numpy.all(hsv_image[120, 0, :] == [0, 1, 0])
+        assert numpy.all(hsv_image[121, 1, :] == [0, 1, 0])
+        assert numpy.all(hsv_image[180, 0, :] == [0, 1, 1])
+        assert numpy.all(hsv_image[181, 1, :] == [0, 1, 1])
+        assert numpy.all(hsv_image[240, 0, :] == [0, 0, 1])
+        assert numpy.all(hsv_image[241, 1, :] == [0, 0, 1])
+        assert numpy.all(hsv_image[300, 0, :] == [1, 0, 1])
+        assert numpy.all(hsv_image[301, 1, :] == [1, 0, 1])
+
 
 @pytest.fixture(scope="session", autouse=True)
 def run_around_tests(request):
@@ -37,6 +72,7 @@ def run_around_tests(request):
     # Code that will run after your test, for example:
     def remove_test_dir():
         if os.path.isdir('result_images'):
-            #shutil.rmtree('result_images')
+            # shutil.rmtree('result_images')
             pass
+
     request.addfinalizer(remove_test_dir)
