a01399a47a0fdf8005d96abf5b25930d718ee187
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-06-25 15:59:44 +0200

    Changed classification for crossing fibers.
    Added small tool to execute classification as a binary. This isn't included in the installation in SLIX yet. Tests will be added soon

--- SLIX/classification.py
+++ SLIX/classification.py
@@ -1,21 +1,24 @@
 import numpy
 
 
-def crossing_mask(high_prominence_peaks, max_image, flat_mask):
+def crossing_mask(high_prominence_peaks, max_image):
     crossing = (high_prominence_peaks == 4) | (high_prominence_peaks == 6)
-    mean_flat_signal = numpy.average(max_image[flat_mask])
-    std_flat_signal = numpy.std(max_image[flat_mask])
-    crossing[(max_image < mean_flat_signal - std_flat_signal) |
-             (max_image > mean_flat_signal + std_flat_signal)] = False
+    mean_flat_signal = numpy.average(max_image)
+    crossing[(max_image < mean_flat_signal)] = False
     return crossing
 
 
 def inclinated_mask(high_prominence_peaks, peakdistance, max_image, flat_mask):
-    resulting_labeling = numpy.zeros(high_prominence_peaks.shape, dtype=numpy.uint8)
-    resulting_labeling[high_prominence_peaks == 1] = 1
+    inclinated_areas = numpy.zeros(high_prominence_peaks.shape, dtype=numpy.uint8)
     mean_flat_signal = numpy.average(max_image[flat_mask])
-    resulting_labeling[(high_prominence_peaks == 2) & (peakdistance < 145) & (max_image > mean_flat_signal)] = 2
-    return resulting_labeling
+    two_peak_mask = (high_prominence_peaks == 2) & (max_image > mean_flat_signal)
+
+    inclinated_areas[flat_mask] = 1
+    inclinated_areas[two_peak_mask & (peakdistance > 120) & (peakdistance < 150)] = 2
+    inclinated_areas[two_peak_mask & (peakdistance < 120)] = 3
+    inclinated_areas[high_prominence_peaks == 1] = 2
+
+    return inclinated_areas
 
 
 def flat_mask(high_prominence_peaks, low_prominence_peaks, peakdistance):
