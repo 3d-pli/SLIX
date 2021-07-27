5939d9f5f9c0b071e984e19bfe335082e511c4c4
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-07-27 13:19:18 +0200

    Tests for classification
    Signed-off-by: Jan André Reuter <jan.andre.reuter@hotmail.de>

--- SLIX/classification.py
+++ SLIX/classification.py
@@ -3,19 +3,23 @@ import numpy
 
 def crossing_mask(high_prominence_peaks, max_image):
     crossing = (high_prominence_peaks == 4) | (high_prominence_peaks == 6)
+    crossing = crossing.astype(numpy.uint8)
     mean_flat_signal = numpy.average(max_image)
-    crossing[(max_image < mean_flat_signal)] = False
+    crossing[(max_image < mean_flat_signal)] = 0
     crossing[high_prominence_peaks == 4] = 1
     crossing[high_prominence_peaks == 6] = 2
     return crossing
 
 
-def inclinated_mask(high_prominence_peaks, peakdistance, max_image, flat_mask):
-    inclinated_areas = numpy.zeros(high_prominence_peaks.shape, dtype=numpy.uint8)
-    mean_flat_signal = numpy.average(max_image[flat_mask])
-    two_peak_mask = (high_prominence_peaks == 2) & (max_image > mean_flat_signal)
+def inclinated_mask(high_prominence_peaks, peakdistance,
+                    max_image, flat_mask):
+    inclinated_areas = numpy.zeros(high_prominence_peaks.shape,
+                                   dtype=numpy.uint8)
+    mean_flat_signal = numpy.average(max_image[flat_mask > 0])
+    two_peak_mask = (high_prominence_peaks == 2) & \
+                    (max_image > mean_flat_signal)
 
-    inclinated_areas[flat_mask] = 1
+    inclinated_areas[flat_mask > 0] = 1
     inclinated_areas[two_peak_mask &
                      (peakdistance > 120) &
                      (peakdistance < 150)] = 2
