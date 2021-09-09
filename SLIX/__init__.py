378626abf42de9c42ab1a7dc4b6730bb3c9f0563
Jan André Reuter <jan.andre.reuter@hotmail.de>
2021-09-09 15:10:02 +0200

    Update version

--- SLIX/__init__.py
+++ SLIX/__init__.py
@@ -1,7 +1,7 @@
 """
 Scattered Light Imaging ToolboX (SLIX) – an open-source Python package that allows a fully automated evaluation of SLI measurements and the generation of different parameter maps
 """
-__version__ = '2.3.0-beta2'
+__version__ = '2.3.0'
 __all__ = ['toolbox', 'io', 'visualization', 'preparation',
            'attributemanager']
 
